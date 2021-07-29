f52b593721a1a4daf9d09c07101e00742cd80a63
Carlos Benjamin Alvarez Cruz <carlosalvarez098@gmail.com>
2021-07-29 09:48:16 -0500

    deployment

---
+++ scripts/deploy_flashswap.js
@@ -0,0 +1,19 @@
+const hre = require("hardhat");
+
+async function main() {
+
+    const Flashswap_Uniswap = await hre.ethers.getContractFactory("Flashswap_uniswap");
+    const flashswap_uniswap = await Flashswap_Uniswap.deploy();
+
+    await flashswap_uniswap.deployed();
+
+    console.log("Liquidation deployed to:", flashswap_uniswap.address);
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
